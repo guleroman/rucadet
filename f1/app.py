06f4d49e23858fcf394c87cd109c5e6bb02f025f
Roman <remembername@protonmail.com>
2019-12-19 03:24:32 +0300

    init

--- f1/app.py
+++ f1/app.py
@@ -2,12 +2,12 @@ from flask import Flask, jsonify, request, json, make_response, send_file,render
 from flask_sslify import SSLify
 
 from OpenSSL import SSL
-context = SSL.Context(SSL.OP_NO_SSLv3)
+context = SSL.Context(SSL.SSLv23_METHOD)
 context.use_privatekey_file('web.key')
 context.use_certificate_file('web.crt')
 
 app = Flask(__name__)
-
+sslify = SSLify(app)
 
 
 @app.route('/')
@@ -18,4 +18,5 @@ def dash_overview_2():
 	return render_template('index.html')
 
 if __name__ == '__main__':
+	context = ('web.crt', 'web.key')
     app.run(host='0.0.0.0', port=80, debug = True, ssl_context = context)
